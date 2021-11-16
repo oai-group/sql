4809ba83c2a96ba77fd440db42b224b4e87f667b
zcq98 <1229896069@qq.com>
2021-11-16 12:40:09 +0800

    网络状态测量和流量识别数据库

---
+++ measure.sql
@@ -0,0 +1,36 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : enb2
+ Source Server Type    : MySQL
+ Source Server Version : 50733
+ Source Host           : 192.168.31.50:3306
+ Source Schema         : mytestdb
+
+ Target Server Type    : MySQL
+ Target Server Version : 50733
+ File Encoding         : 65001
+
+ Date: 22/10/2021 13:04:02
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for measure
+-- ----------------------------
+DROP TABLE IF EXISTS `measure`;
+CREATE TABLE `measure`  (
+  `srcIP` bigint(20) UNSIGNED ZEROFILL NOT NULL,
+  `dstIP` bigint(20) UNSIGNED NOT NULL,
+  `srcPort` smallint(6) UNSIGNED NOT NULL,
+  `dstPort` smallint(6) UNSIGNED NOT NULL,
+  `protocol` tinyint(4) UNSIGNED NOT NULL,
+  `averBytes` double(20, 1) NULL DEFAULT NULL,
+  `averPkts` double(20, 1) NULL DEFAULT NULL,
+  `flowType` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`srcIP`, `dstIP`, `srcPort`, `dstPort`, `protocol`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
+
+SET FOREIGN_KEY_CHECKS = 1;
---
+++ total_recv.sql
@@ -0,0 +1,32 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : enb2
+ Source Server Type    : MySQL
+ Source Server Version : 50733
+ Source Host           : 192.168.31.50:3306
+ Source Schema         : mytestdb
+
+ Target Server Type    : MySQL
+ Target Server Version : 50733
+ File Encoding         : 65001
+
+ Date: 22/10/2021 13:04:18
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for total
+-- ----------------------------
+DROP TABLE IF EXISTS `total_recv`;
+CREATE TABLE `total_recv`  (
+  `time` int(20) UNSIGNED NOT NULL,
+  `totalBytes` double(20, 2) UNSIGNED NULL DEFAULT NULL,
+  `totalPkts` double(20, 2) UNSIGNED NULL DEFAULT NULL,
+  `data_time_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`time`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
